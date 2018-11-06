d5dfb7ed92ab7702ecc3ef4b95837116a5cafb09
WaynePeng <pengwayne@outlook.com>
2018-11-06 11:04:07 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<style lang="less">
+  @import "./index.less";
+</style>
+<template>
+  <div class="index">
+    this is index !
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ plugins/axios.js
@@ -0,0 +1,3 @@
+export default function ({ $axios}) {
+  $axios.defaults.baseURL = 'http://118.25.16.238:8080';
+}
